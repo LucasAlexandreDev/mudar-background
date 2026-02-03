7ca5b22bb516ddcdc8b87c4c102e4e18c80a6622
Lucas Alexandre <lucasalexandre.lads13@gmail.com>
2026-02-02 22:07:23 -0300

    feat(style): adiciona animações e imagem ao layout

--- main.js
+++ main.js
@@ -1,45 +1,51 @@
-'use strict' // faz o sistema ser mais rigoroso com o código,
-            //transformando as sintaxes erradass aceitas anteriormente, agora não são mais
+    // faz o sistema ser mais rigoroso com o código,
+    // corrigindo sintaxes erradas que antes eram aceitas, mas agora não são mais
+    'use strict';   
 
-/* 
-fn ->   recebe como valor a própria função (especifica somente para funções)
-fn = function(){a = 10}
-*/
+    /* 
 
-// Seleciona o conjunto pelo ID que possui no HTML (botão)
-const botaoTrocaCor = document.getElementById('botao-trocar-cor')
+    fn = function(){a = 10}
+    fn ->   recebe como valor a própria função (especifica somente para funções)
 
-function trocarCorFundo(){
+    */
 
-    // Serve para pegar o campo de texto (cor) que o usuário digitar 
-    // value -> serve para pegar o conteúdo dentro do conjunto  
-    const corUsuario = document.getElementById('cor-usuario').value
-    
-    // Tradução das cores 
-   
-    if (corUsuario == 'vermelho'){
-        document.documentElement.style.setProperty('--color-bg', 'red')
-    }else if (corUsuario == 'verde'){
-        document.documentElement.style.setProperty('--color-bg', 'green')
-    }else if(corUsuario == 'amarelo'){
-        document.documentElement.style.setProperty('--color-bg', 'yellow')
-    }else if(corUsuario == 'azul'){
-        document.documentElement.style.setProperty('--color-bg', 'blue' )
-    }else{
-        document.documentElement.style.setProperty('--color-bg', corUsuario)
-    }
-}
-
-// Serve para escutar eventos (click nesse caso)
-botaoTrocaCor.addEventListener('click', trocarCorFundo )
-
-
-function trocarCorBotaoVerde(){
+    // Seleção dos elementos por ID
+    const botaoTrocaCor = document.getElementById('botao-trocar-cor')
     const corVerde = document.getElementById('botao-verde')
+    const corVermelho = document.getElementById('botao-vermelho')
 
-}
+    // função pra validar e aplicar as cores do input 
+    function trocarCorFundo(){
+        
+        // value -> serve para pegar o valor atual de um elemento
+        const corUsuario = document.getElementById('cor-usuario').value   
 
+        // Validação das cores em pt-br 
+        if (corUsuario == 'vermelho'){
+            document.documentElement.style.setProperty('--color-bg', 'red')
+        }else if (corUsuario == 'verde'){
+            document.documentElement.style.setProperty('--color-bg', 'green')
+        }else if(corUsuario == 'amarelo'){
+            document.documentElement.style.setProperty('--color-bg', 'yellow')
+        }else if(corUsuario == 'azul'){
+            document.documentElement.style.setProperty('--color-bg', 'blue' )
+        }else{
+            document.documentElement.style.setProperty('--color-bg', corUsuario)
+        }
+    }
 
+    // Cada função muda a variável do css '--color-bg' para o nome da sua respectiva cor 
+    function trocarCorVerde(){
+        document.documentElement.style.setProperty('--color-bg', 'green')
+    }
 
+    function trocarCorVermelho(){
+        document.documentElement.style.setProperty('--color-bg', 'red')
+    }
 
+    // Serve para escutar eventos e verificar o 'click' nessa instância  
+    botaoTrocaCor.addEventListener('click', trocarCorFundo)
+    corVerde.addEventListener('click', trocarCorVerde)
+    corVermelho.addEventListener('click', trocarCorVermelho)
 
+ 
