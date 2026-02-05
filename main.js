8cb3d3e444430372eda2ef9e20e57a341425de91
Lucas Alexandre <lucasalexandre.lads13@gmail.com>
2026-02-05 18:39:38 -0300

    feat: adiciona botões (cor aleatória e resetar cor)

--- main.js
+++ main.js
@@ -2,60 +2,69 @@
     // corrigindo sintaxes erradas que antes eram aceitas, mas agora não são mais
     'use strict';   
 
-    /* 
-
-    fn = function(){a = 10}
-    fn ->   recebe como valor a própria função (especifica somente para funções)
-
-    */
-
     // Seleção dos elementos por ID
     const botaoTrocaCor = document.getElementById('botao-trocar-cor')
     const corVerde = document.getElementById('botao-verde')
     const corVermelho = document.getElementById('botao-vermelho')
-
-    // array das cores validas - primárias
-    const coresPrimarias = ['vermelho', 'azul', 'amarelo'];
-
+    const corAleatoria = document.getElementById('botao-aleatorio-cor')
+    const corRestart = document.getElementById('botao-restart-cor')
+    const corTextoAtual = document.getElementById('cor-atual')
 
     // função pra validar e aplicar as cores do input 
     function trocarCorFundo(){
         
-        // value -> serve para pegar o valor atual de um elemento
-        const corUsuario = document.getElementById('cor-usuario').value   
+      // value -> serve para pegar o valor atual de um elemento
+      const corUsuario = document.getElementById('cor-usuario').value
+   
+      // Validação do campo vazio 
+      if (corUsuario == '') {
+        alert('Erro: O campo da cor não pode ficar vazio!!!')
+            
+      // validação somente das cores primárias em pt-br
+      }else if (corUsuario == 'vermelho'){
+        document.documentElement.style.setProperty('--color-bg', 'red')
 
-        // Validação do campo vazio 
-        if (corUsuario === '') {
-            alert('ERRO: O campo da cor não pode ficar vazio!!!')
+      }else if(corUsuario == 'amarelo'){
+        document.documentElement.style.setProperty('--color-bg', 'yellow')
 
-        // validação das cores válidas traduzidas - cores primárias
-        }else if (!coresPrimarias.includes(corUsuario)){
-            alert('ERRO: Cor inválida!!!. Use apenas as cores primárias: vermelho, azul ou amarelo')
+      }else if(corUsuario == 'azul'){
+        document.documentElement.style.setProperty('--color-bg', 'blue' )
+      
+      // Se não for uma das cores em pt-br acima, testa se é uma cor válida em inglês ou código hexadecimal
+      // Verificando se o navegador reconhece o valor digitado como uma cor 'CSS' válida
+      } else if (CSS.supports('color', corUsuario)) {
+        document.documentElement.style.setProperty('--color-bg', corUsuario)
 
-        }else if (corUsuario == 'vermelho'){
-            document.documentElement.style.setProperty('--color-bg', 'red')
-
-        }else if(corUsuario == 'amarelo'){
-            document.documentElement.style.setProperty('--color-bg', 'yellow')
-
-        }else if(corUsuario == 'azul'){
-            document.documentElement.style.setProperty('--color-bg', 'blue' )
-
-        }else{
-            document.documentElement.style.setProperty('--color-bg', corUsuario)
-        }
+      }else{
+        alert('Erro: Cor inválida. Digite uma cor primária ou uma cor em inglês!!!')
+      }
     }
 
     // Cada função muda a variável do css '--color-bg' para o nome da sua respectiva cor 
     function trocarCorVerde(){
-        document.documentElement.style.setProperty('--color-bg', 'green')
+      document.documentElement.style.setProperty('--color-bg', 'green')
     }
 
     function trocarCorVermelho(){
-        document.documentElement.style.setProperty('--color-bg', 'red')
+      document.documentElement.style.setProperty('--color-bg', 'red')
     }
 
+    function trocarCorAleatoria() {
+      const r = Math.floor(Math.random() * 256)
+      const g = Math.floor(Math.random() * 256)
+      const b = Math.floor(Math.random() * 256)
+
+      const corFinal = `rgb(${r}, ${g}, ${b})`
+      document.documentElement.style.setProperty('--color-bg', corFinal)
+    }
+
+    function trocarCorRestart(){
+      document.documentElement.style.setProperty('--color-bg', 'white')
+    }
     // Serve para escutar eventos e verificar o 'click' nessa instância  
     botaoTrocaCor.addEventListener('click', trocarCorFundo)
     corVerde.addEventListener('click', trocarCorVerde)
     corVermelho.addEventListener('click', trocarCorVermelho)
+    corAleatoria.addEventListener('click', trocarCorAleatoria)
+    corRestart.addEventListener('click', trocarCorRestart)
+    
